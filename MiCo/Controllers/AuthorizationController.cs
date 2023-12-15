d931f94bc74d4ed45d6a821ebc00973b800253b5
Błażej <hunter13cie@gmail.com>
2023-12-15 16:30:15 +0100

    Login and registration views

---
+++ MiCo/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiCo.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
