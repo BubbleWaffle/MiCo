bb530bd2f45d6f7ce766ae9f8a375f89ecd14c03
Błażej <hunter13cie@gmail.com>
2023-12-23 18:12:09 +0100

    _layout view changes and new icon

--- MiCo/Controllers/JusticeController.cs
+++ MiCo/Controllers/JusticeController.cs
@@ -4,8 +4,12 @@ namespace MiCo.Controllers
 {
     public class JusticeController : Controller
     {
+        [Route("/Justice/Index")]
         public IActionResult Index()
         {
+            if (!HttpContext.Session.TryGetValue("UserId", out _) || HttpContext.Session.GetInt32("Role") != 1 )
+                return RedirectToAction("Index", "Home");
+
             return View();
         }
     }
